661b1a5dd7b23aa6a28f391dbe8645df4b130b06
ben-drohealth <bennyblasco@gmail.com>
2021-06-17 06:45:52 +0100

    Second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+        backgroundColor: Colors.teal[900],
+        body: SafeArea(
+          child:
+              Container(child: Text('My name is Daziba'), 
+              color: Colors.blue),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
